2aadf7c3b9a12d466d33052fefc3eff2f526497f
Benjamin Volodarsky <benny.volo28@gmail.com>
2024-01-28 00:22:42 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# udemy
+Udemy project
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        {/* <Navbar /> */}
+        <Routes>
+          {/* <Route path="/" element={<LandingPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
