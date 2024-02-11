d3bbb1e9cce70e7aa6e530d9e8e8fe37ba8305e6
Benjamin Volodarsky <benny.volo28@gmail.com>
2024-02-11 14:54:09 +0200

    .

--- client/src/Components/Courses/Course.tsx
+++ client/src/Components/Courses/Course.tsx
@@ -1,60 +1,80 @@
-
 export interface CourseProps {
-    courseId: number,
-    teacherId: number,
-    courseName: string,
-    teacherName: string,
-    mainDescription: string,
-    rating: number,
-    numberOfRatings: number,
-    numberOfStudents: number,
-    lastUpdated: Date,
-    language: string,
-    subtitlesLanguage: { type: string, default: 'English' },
-    fullPrice: number,
-    discountPrice: number,
-    secondDescriptions: [string],
-    courseDuration: number,
-    articlesNumber: number,
-    downloadableResourcesNumber: number,
-    courseContent: string,
-    requirements: [string],
-    fullDescription: string,
-    course_img: string
+  courseId: number;
+  teacherId: number;
+  courseName: string;
+  teacherName: string;
+  mainDescription: string;
+  rating: number;
+  numberOfRatings: number;
+  numberOfStudents: number;
+  lastUpdated: Date;
+  language: string;
+  subtitlesLanguage: { type: string; default: "English" };
+  fullPrice: number;
+  discountPrice: number;
+  secondDescriptions: [string];
+  courseDuration: number;
+  articlesNumber: number;
+  downloadableResourcesNumber: number;
+  courseContent: string;
+  requirements: [string];
+  fullDescription: string;
+  course_img: string;
 }
-const Course = ({ img, title, teacher, rating, price, tag, numberOfRatings, id }: { img: string, title: string, teacher: string, rating: number, price: number, tag: string, numberOfRatings: number, id: number }) => {
-    const starColor = () => {
-        const stars = document.querySelectorAll('.star') as NodeListOf<HTMLElement>;;
-        if (stars.length > 0) {
-            stars.forEach((star, index) => {
-
-                if (index >= 5) index = 0;
-                if (index + 1 <= rating) star.style.color = '#B4690E';
-            })
-        }
-    };
-    starColor();
-    return (
-        <div>
-            <a href={`/course/${id}`}>
-                <img src={img} alt="" />
-                <h2>{title}</h2>
-                <p>{teacher}</p>
-                <p>{rating.toFixed(2)}&nbsp;
-                    <span className="star">&#9733;</span>
-                    <span className="star">&#9733;</span>
-                    <span className="star">&#9733;</span>
-                    <span className="star">&#9733;</span>
-                    <span className="star">&#9733;</span>&nbsp;({numberOfRatings})</p>
-                <p>${Math.round(Number(price))}</p>
-                <p>{tag}</p>
-            </a>
+const Course = ({
+  img,
+  title,
+  teacher,
+  rating,
+  price,
+  tag,
+  numberOfRatings,
+  id,
+}: {
+  img: string;
+  title: string;
+  teacher: string;
+  rating: number;
+  price: number;
+  tag: string;
+  numberOfRatings: number;
+  id: number;
+}) => {
+  const starColor = () => {
+    const stars = document.querySelectorAll(".star") as NodeListOf<HTMLElement>;
+    if (stars.length > 0) {
+      stars.forEach((star, index) => {
+        if (index >= 5) index = 0;
+        if (index + 1 <= rating) star.style.color = "#B4690E";
+      });
+    }
+  };
+  starColor();
+  return (
+    <div className=" h-full w-full">
+      <a href={`/course/${id}`}>
+        <img className=" h-[9.6rem] w-full" src={img} alt="" />
+        <h2 className=" font-bold text-[1.1rem] text-slate-800">{title}</h2>
+        <p className=" text-xs font-light text-slate-800">{teacher}</p>
+        <p>
+          {rating.toFixed(2)}&nbsp;
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <span className="star">&#9733;</span>&nbsp;({numberOfRatings})
+        </p>
+        <p className=" font-bold tracking-tight text-[1rem] mt-1">
+          ${Math.round(Number(price))}
+        </p>
+        <div className=" w-full h-fit flex flex-row items-center justify-start mt-2">
+          <div className=" text-center w-fit px-4 text-xs font-bold text-slate-700 bg-Udemyyellow-200">
+            {tag}
+          </div>
         </div>
+      </a>
+    </div>
+  );
+};
 
-    )
-
-
-}
-
-
-export default Course
+export default Course;
--- client/src/Components/Courses/Courses.tsx
+++ client/src/Components/Courses/Courses.tsx
@@ -1,19 +1,39 @@
-import Course, { CourseProps } from './Course';
-const Courses = ({ type, componentsTitle }: { type: any, componentsTitle: string }) => {
-    if (type === null || type === undefined) return;
-    console.log(componentsTitle)
-    return (
-        <div>
-            <strong><h2 style={{ paddingLeft: "20px" }}>{componentsTitle}</h2></strong>
-            <div style={{ display: "flex", gap: "20px", padding: "20px" }}>
-                {type.map((course: CourseProps, index: number) => (
-                    <div key={index}>
-                        <Course img={course.course_img} title={course.courseName} teacher={course.teacherName} rating={course.rating} price={course.fullPrice}
-                            tag={course.language} numberOfRatings={course.numberOfRatings} id={course.courseId} />
-                    </div>
-                ))}
-            </div></div>
-    )
-}
+import Course, { CourseProps } from "./Course";
+const Courses = ({
+  type,
+  componentsTitle,
+}: {
+  type: any;
+  componentsTitle: string;
+}) => {
+  if (type === null || type === undefined) return;
+  console.log(componentsTitle);
 
-export default Courses
+  return (
+    <div className=" flex flex-row justify-center items-center w-full h-fit mt-6">
+      <div className=" flex flex-col w-fit h-full justify-start items-start">
+        <strong>
+          <h2 className=" font-bold text-[1.7rem] pl-5 text-slate-900">{componentsTitle}</h2>
+        </strong>
+        <div className=" flex gap-5 p-5 h-[21rem] w-[95rem]">
+          {type.map((course: CourseProps, index: number) => (
+            <div className=" h-full flex-1 flex-grow" key={index}>
+              <Course
+                img={course.course_img}
+                title={course.courseName}
+                teacher={course.teacherName}
+                rating={course.rating}
+                price={course.fullPrice}
+                tag={course.language}
+                numberOfRatings={course.numberOfRatings}
+                id={course.courseId}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Courses;
