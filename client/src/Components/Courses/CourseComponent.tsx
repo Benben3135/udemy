d65ed8851823909cac8eabb6b0ecb19179033ed1
navered <128045047+navered@users.noreply.github.com>
2024-02-24 17:48:04 +0200

    push

---
+++ client/src/Components/Courses/CourseComponent.tsx
@@ -0,0 +1,14 @@
+// // CourseComponent.js
+// import React from 'react';
+
+// const CourseComponent = ({ course }) => {
+//   // הוספת עיצוב ולוגיקה שמתאימים לקומפוננטה החדשה
+//   return (
+//     <div className="course-component">
+//       <h2>{course.courseName} Component</h2>
+//       {/* הוספת פרטים ועיצוב נוסף כרצונך */}
+//     </div>
+//   );
+// };
+
+// export default CourseComponent;
